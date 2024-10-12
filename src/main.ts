877ce8d07d9d207a7951de8573796e5419a26f54
Davis Owen <sdowen12@gmail.com>
2024-10-12 00:49:07 -0500

    completed multi page functionality

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Plugin, Notice, MarkdownView } from "obsidian";
-import { AutoClassifierSettingTab, AutoClassifierSettings, DEFAULT_SETTINGS, OutLocation, OutType} from "src/settings";
+import { Plugin, Notice, MarkdownView, TFile, WorkspaceLeaf } from "obsidian";
+import { AutoClassifierSettingTab, AutoClassifierSettings, DEFAULT_SETTINGS, OutLocation, OutType, CommandOption} from "src/settings";
 import { ViewManager } from "src/view-manager";
 import { ChatGPT } from 'src/api';
 
@@ -113,6 +113,142 @@ export default class AutoClassifierPlugin extends Plugin {
 			console.log(`Created note: ${noteTitle}`);
 		}
 	}
+
+	async closeLeaf(leaf: WorkspaceLeaf) {
+		if (leaf) {
+			leaf.detach();
+		}
+	}
+
+	async classifyFile(
+		inputType: InputType,
+		file: TFile,
+		wholeVault: boolean,
+		commandOption: CommandOption,
+		refs: string[],
+		signal: AbortSignal
+	) {
+		// open file in vault in new tab for processing - if wholeVault
+		// is false, we should just stay in the existing file
+		const leaf = this.app.workspace.getLeaf();
+		await leaf.openFile(file);
+
+		// Set Input 
+		let inputs: string[] | null = [''];
+		if (inputType == InputType.SelectedArea) {
+			inputs = await this.viewManager.getSelection();
+		}
+		else if (inputType == InputType.Title) {
+			inputs = await this.viewManager.getTitle();
+		}
+		else if (inputType == InputType.FrontMatter) {
+			inputs = await this.viewManager.getFrontMatter();
+		}
+		else if (inputType == InputType.Content) {
+			inputs = await this.viewManager.getContent();
+		}
+		else if (inputType == InputType.CalloutContent) {
+			inputs = await this.viewManager.getCalloutContent();
+		}
+
+		// input error
+		if (!inputs) {
+			if (!wholeVault) {
+				new Notice(`⛔ ${this.manifest.name}: no input data`);
+			}
+			return;
+		}
+
+		for (let input of inputs) {
+			// input error
+			if (!input) {
+				new Notice(`⛔ ${this.manifest.name}: no input data`);
+				return;
+			}
+
+			// Replace {{input}}, {{reference}}
+			let user_prompt = commandOption.prmpt_template;
+			user_prompt = user_prompt.replace('{{input}}', input);
+			user_prompt = user_prompt.replace('{{reference}}', refs.join(','));
+			user_prompt = user_prompt.replace('{{max_tags}}', commandOption.max_tags === 0 ? 'unlimited' : commandOption.max_tags.toString());
+
+			const system_role = commandOption.chat_role;
+
+			// ------- [API Processing] -------
+			// Call API
+			let responseRaw;
+			try {
+				responseRaw = await ChatGPT.callAPI(
+					system_role, 
+					user_prompt, 
+					this.settings.apiKey,
+					signal,
+					this.settings.commandOption.model,
+					this.settings.commandOption.max_tokens,
+				);
+				if (!responseRaw) {
+					new Notice(`⛔ ${this.manifest.name}: empty API response`);
+					return null;
+				}
+			} catch (error) {
+				new Notice(`⛔ ${this.manifest.name}: API error (output ${error})`);
+				return null;
+			}
+			let jsonList;
+			try {
+				jsonList = JSON.parse(responseRaw);
+				if (!Array.isArray(jsonList)) {
+					throw new Error();
+				}
+			} catch (error) {
+				console.error(error);
+				const errorString = `⛔ ${this.manifest.name}: output format error (output: ${responseRaw})`;
+				console.error(errorString);
+				new Notice(errorString);
+				return null;
+			}
+			let tagString = ' #auto-classifier ';
+			for (let response of jsonList) {
+				if (!response || !response.reliability || !response.output) {
+					new Notice(`⛔ ${this.manifest.name}: response format error`);
+					return;
+				}
+				// Avoid low reliability
+				if (response.reliability <= 0.2) {
+					new Notice(`⛔ ${this.manifest.name}: response has low reliability (${response.reliability})`);
+					continue;
+				}
+				const outputName = response.output + '-GPT';
+				const output = this.viewManager.preprocessOutput(outputName, commandOption.outType, commandOption.outPrefix, commandOption.outSuffix);
+				await this.createNoteIfNotExist(outputName);
+				tagString += output + ' '
+			}
+
+			// ------- [Add Tag] -------
+			// Output Type 1. [Tag Case] + Output Type 2. [Wikilink Case]
+			if (commandOption.outType == OutType.Tag || commandOption.outType == OutType.Wikilink) {
+				if (commandOption.outLocation == OutLocation.Cursor) {
+					await this.viewManager.insertAtCursor(tagString, commandOption.overwrite);
+				} 
+				else if (commandOption.outLocation == OutLocation.ContentTop) {
+					await this.viewManager.insertAtContentTop(tagString);
+				}
+				else if (commandOption.outLocation == OutLocation.CalloutTop) {
+					await this.viewManager.insertAtCalloutTop(input, tagString);
+				}
+			}
+			// Output Type 3. [Frontmatter Case]
+			else if (commandOption.outType == OutType.FrontMatter) {
+				await this.viewManager.insertAtFrontMatter(commandOption.key, tagString, commandOption.overwrite, commandOption.outPrefix, commandOption.outSuffix);
+			}
+			// Output Type 4. [Title]
+			else if (commandOption.outType == OutType.Title) {
+				await this.viewManager.insertAtTitle(tagString, commandOption.overwrite, commandOption.outPrefix, commandOption.outSuffix);
+			}
+			new Notice(`✅ ${this.manifest.name}: classified to ${tagString}`);
+		}
+	}
+
 	// Main Classification
 	async classifyTag(inputType: InputType, signal: AbortSignal, wholeVault: boolean = false) {
 		const commandOption = this.settings.commandOption;
@@ -133,125 +269,29 @@ export default class AutoClassifierPlugin extends Plugin {
 		const files = wholeVault ? this.app.vault.getMarkdownFiles() : [this.app.workspace.getActiveViewOfType(MarkdownView)?.file]
 
 		for (const file of files) {
+			if (signal.aborted) {
+				throw new Error('Aborted')
+			}
+
 			if (!file) {
 				new Notice(`⛔ ${this.manifest.name}: undefined file found`);
-				return null
-			}
-			// switch to another file in vault for processing - if wholeVault
-			// is false, we should just stay in the existing file
-			await this.app.workspace.openLinkText(file.path, '' , true);
+			} else {
+				// Open file in vault in new tab for processing - if wholeVault
+				// is false, we should just stay in the existing file
+				const leaf = this.app.workspace.getLeaf();
+				await leaf.openFile(file);
 
-			// Set Input 
-			let inputs: string[] | null = [''];
-			if (inputType == InputType.SelectedArea) {
-				inputs = await this.viewManager.getSelection();
-			}
-			else if (inputType == InputType.Title) {
-				inputs = await this.viewManager.getTitle();
-			}
-			else if (inputType == InputType.FrontMatter) {
-				inputs = await this.viewManager.getFrontMatter();
-			}
-			else if (inputType == InputType.Content) {
-				inputs = await this.viewManager.getContent();
-			}
-			else if (inputType == InputType.CalloutContent) {
-				inputs = await this.viewManager.getCalloutContent();
-			}
+				await this.classifyFile(
+					inputType,
+					file,
+					wholeVault,
+					commandOption,
+					refs,
+					signal
+				);
 
-			// input error
-			if (!inputs) {
-				new Notice(`⛔ ${this.manifest.name}: no input data`);
-				return null;
-			}
-
-			for (let input of inputs) {
-				// input error
-				if (!input) {
-					new Notice(`⛔ ${this.manifest.name}: no input data`);
-					return null;
-				}
-
-				// Replace {{input}}, {{reference}}
-				let user_prompt = commandOption.prmpt_template;
-				user_prompt = user_prompt.replace('{{input}}', input);
-				user_prompt = user_prompt.replace('{{reference}}', refs.join(','));
-				user_prompt = user_prompt.replace('{{max_tags}}', commandOption.max_tags === 0 ? 'unlimited' : commandOption.max_tags.toString());
-
-				const system_role = commandOption.chat_role;
-
-				// ------- [API Processing] -------
-				// Call API
-				let responseRaw;
-				try {
-					responseRaw = await ChatGPT.callAPI(
-						system_role, 
-						user_prompt, 
-						this.settings.apiKey,
-						signal,
-						this.settings.commandOption.model,
-						this.settings.commandOption.max_tokens,
-					);
-					if (!responseRaw) {
-						new Notice(`⛔ ${this.manifest.name}: empty API response`);
-						return null;
-					}
-				} catch (error) {
-					new Notice(`⛔ ${this.manifest.name}: API error (output ${error})`);
-					return null;
-				}
-				let jsonList;
-				try {
-					jsonList = JSON.parse(responseRaw);
-					if (!Array.isArray(jsonList)) {
-						throw new Error();
-					}
-				} catch (error) {
-					console.error(error);
-					const errorString = `⛔ ${this.manifest.name}: output format error (output: ${responseRaw})`;
-					console.error(errorString);
-					new Notice(errorString);
-					return null;
-				}
-				let tagString = ' #auto-classifier ';
-				for (let response of jsonList) {
-					if (!response || !response.reliability || !response.output) {
-						new Notice(`⛔ ${this.manifest.name}: response format error`);
-						return;
-					}
-					// Avoid low reliability
-					if (response.reliability <= 0.2) {
-						new Notice(`⛔ ${this.manifest.name}: response has low reliability (${response.reliability})`);
-						continue;
-					}
-					const outputName = response.output + '-GPT';
-					const output = this.viewManager.preprocessOutput(outputName, commandOption.outType, commandOption.outPrefix, commandOption.outSuffix);
-					await this.createNoteIfNotExist(outputName);
-					tagString += output + ' '
-				}
-
-				// ------- [Add Tag] -------
-				// Output Type 1. [Tag Case] + Output Type 2. [Wikilink Case]
-				if (commandOption.outType == OutType.Tag || commandOption.outType == OutType.Wikilink) {
-					if (commandOption.outLocation == OutLocation.Cursor) {
-						await this.viewManager.insertAtCursor(tagString, commandOption.overwrite);
-					} 
-					else if (commandOption.outLocation == OutLocation.ContentTop) {
-						await this.viewManager.insertAtContentTop(tagString);
-					}
-					else if (commandOption.outLocation == OutLocation.CalloutTop) {
-						await this.viewManager.insertAtCalloutTop(input, tagString);
-					}
-				}
-				// Output Type 3. [Frontmatter Case]
-				else if (commandOption.outType == OutType.FrontMatter) {
-					await this.viewManager.insertAtFrontMatter(commandOption.key, tagString, commandOption.overwrite, commandOption.outPrefix, commandOption.outSuffix);
-				}
-				// Output Type 4. [Title]
-				else if (commandOption.outType == OutType.Title) {
-					await this.viewManager.insertAtTitle(tagString, commandOption.overwrite, commandOption.outPrefix, commandOption.outSuffix);
-				}
-				new Notice(`✅ ${this.manifest.name}: classified to ${tagString}`);
+				// Close tab when done
+				this.closeLeaf(leaf);
 			}
 		}
 	}
